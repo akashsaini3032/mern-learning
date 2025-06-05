645539dee33c932a98d2998ee665e9e2b257f1e8
akashsaini3032 <akashsainira@gmail.com>
2025-06-05 23:13:13 +0530

    update folder

--- login-register/backend/routes/userRoutes.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const route = express.Router();
-const UserController = require("../controllers/userController")
-route.post("/registration" , UserController.registerdata );
-
-module.exports=route;
--- login-register/login-register/src/components/Header.jsx
+++
@@ -1,8 +0,0 @@
-const Header=()=>{
-    return(
-        <>
-        <h1>Welcome to login and registration page</h1>
-        </>
-    )
-}
-export default Header;
